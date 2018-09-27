3f8fe863699bb40912448fb7c5c9a3a2ee82fc72
Selcuk Karakus <selchuck.karakus@gmail.com>
2018-09-27 21:27:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -29,6 +29,7 @@ function currentLine(katzDeliLine) {
   if(!katzDeliLine.legth) {
   return "The line is currently empty.";
   }
+  let katz = [];
   for(var i = 0; i < katzDeliLine.length; i++) {
     return "The line is currently:"
   }
