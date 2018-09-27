1cfc5f93106864559f4a15451ccf9cd4db1d7952
Selcuk Karakus <selchuck.karakus@gmail.com>
2018-09-27 20:50:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-function takeANumber(currentLine, newPersonName ) {
- currentLine.push(newPersonName);
-  return "Welcome, " + newPersonName + ". You are number " + currentLine.length + " in line.";
+function takeANumber(katzDeliLine, newPersonName ) {
+ katzDeliLine.push(newPersonName);
+  return "Welcome, " + newPersonName + ". You are number " + katzDeliLine.length + " in line.";
 }
 
 function nowServing(katzDeliLine) {
