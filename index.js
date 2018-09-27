5af459a02ba5b517c2666418dcefab62b7a52c3d
Selcuk Karakus <selchuck.karakus@gmail.com>
2018-09-27 20:55:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-function takeANumber(katzDeliLine, newPersonName ) {
- katzDeliLine.push(newPersonName);
-  return "Welcome, " + newPersonName + ". You are number " + katzDeliLine.length + " in line.";
+function takeANumber(deliLine, newPersonName ) {
+ deliLine.push(newPersonName);
+  return "Welcome, " + newPersonName + ". You are number " + deliLine.length + " in line.";
 }
 
-function nowServing(katzDeliLine) {
-  if(katzDeliLine.length === 0){
+function nowServing(deliLine) {
+  if(deliLine.length === 0){
     
   return "There is nobody waiting to be served!" ;
 
   } else {
-    var newPersonName = katzDeliLine[0];
-    katzDeliLine.splice(0,1);
+    var newPersonName = deliLine[0];
+    deliLine.splice(0,1);
   }
-  return "Currently serving " + katzDeliLine + ".";
+  return "Currently serving " + deliLine + ".";
 }
