757f139cc7eba4d5263eca5057bb03a125e4074e
Selcuk Karakus <selchuck.karakus@gmail.com>
2018-09-27 21:18:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ function nowServing(katzDeliLine) {
   return "There is nobody waiting to be served!" ;
 
   } else {
-    return "Currently serving " + deliLine.shift() + ".";
+    return "Currently serving " + katzDeliLine.shift() + ".";
   }
   
 }
