11d3a677cce058147672ba558e4c3e4b3e20b9e9
Selcuk Karakus <selchuck.karakus@gmail.com>
2018-09-27 21:27:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -31,6 +31,6 @@ function currentLine(katzDeliLine) {
   }
   let katz = [];
   for(var i = 0; i < katzDeliLine.length; i++) {
-    return "The line is currently:"
-  }
+  }    return "The line is currently:"
+
 }
